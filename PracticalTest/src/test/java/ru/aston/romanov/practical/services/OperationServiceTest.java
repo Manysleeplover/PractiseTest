dcba0fff40f993068729c324871a4ba2d429b23b
ioromanov <manysleeplover@bk.ru>
2023-10-17 13:39:50 +0300

    OperationService - 100% покрытие тестами

--- PracticalTest/src/test/java/ru/aston/romanov/practical/services/OperationServiceTest.java
+++ PracticalTest/src/test/java/ru/aston/romanov/practical/services/OperationServiceTest.java
@@ -1,43 +1,182 @@
 package ru.aston.romanov.practical.services;
 
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
 import ru.aston.romanov.practical.constants.OperationEnum;
 import ru.aston.romanov.practical.dto.OperationRequestDTO;
+import ru.aston.romanov.practical.dto.TransactionDTO;
+import ru.aston.romanov.practical.entities.Account;
+import ru.aston.romanov.practical.entities.Beneficiary;
+import ru.aston.romanov.practical.exceptions.InsufficientFundsException;
+import ru.aston.romanov.practical.exceptions.InvalidPinCodeException;
+import ru.aston.romanov.practical.exceptions.NoAccountPresentException;
 import ru.aston.romanov.practical.exceptions.UnsupportedAccountOperationException;
+import ru.aston.romanov.practical.repositories.AccountRepo;
 import ru.aston.romanov.practical.services.operations.AccountOperation;
+import ru.aston.romanov.practical.services.operations.DepositOperation;
+import ru.aston.romanov.practical.services.operations.TransferOperation;
+import ru.aston.romanov.practical.services.operations.WithdrawOperation;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class OperationServiceTest {
 
-    @Autowired
-    private Map<String, AccountOperation> operations;
+    private final Map<String, AccountOperation> mapMock = new HashMap<>();
+    @Mock
+    private AccountRepo accountRepo = Mockito.mock(AccountRepo.class);
+    private final ModelMapper modelMapper = new ModelMapper();
 
-    @Autowired
     private OperationService operationService;
 
+    @BeforeEach
+    void initializeOperationMap() {
+        Beneficiary beneficiaryIvan = Beneficiary.builder().id(1L).firstname("Ivan").lastname("Ivanov").patronymic("Ivanovich").pin("1234").build();
+        Account accountIvan = Account.builder().id(1L).balance(BigDecimal.valueOf(20000)).build();
+        beneficiaryIvan.addAccount(accountIvan);
+
+        Beneficiary beneficiaryPetr = Beneficiary.builder().id(2L).firstname("Petrov").lastname("Petr").patronymic("Petrovich").build();
+        Account accountPetr = Account.builder().id(2L).balance(BigDecimal.valueOf(20000)).build();
+        beneficiaryPetr.addAccount(accountPetr);
+
+        when(accountRepo.findById(1L)).thenReturn(Optional.ofNullable(accountIvan));
+        when(accountRepo.findById(2L)).thenReturn(Optional.ofNullable(accountPetr));
 
 
-    @ParameterizedTest
-    @EnumSource(OperationEnum.class)
-    void processOperation_with_valid_operations_name(OperationEnum operationEnum) {
-        String name = operationEnum.getName();
-        assertTrue(operations.containsKey(name));
+        var depositOperation = new DepositOperation(accountRepo, modelMapper);
+        var withdrawOperation = new WithdrawOperation(accountRepo, modelMapper);
+        var transferOperation = new TransferOperation(accountRepo, modelMapper);
+
+        mapMock.put(OperationEnum.WITHDRAW.getName(), withdrawOperation);
+        mapMock.put(OperationEnum.DEPOSIT.getName(), depositOperation);
+        mapMock.put(OperationEnum.TRANSFER.getName(), transferOperation);
+
+
+        operationService = new OperationService(mapMock);
+
+    }
+
+    @Test
+    void givenInsufficientFundsForTransfer_thenNoAccountPresentException() {
+        OperationRequestDTO operationRequestDTO = OperationRequestDTO.builder()
+                .id(1L)
+                .operationType(OperationEnum.TRANSFER.getName())
+                .operationSum(BigDecimal.valueOf(30000))
+                .pin("1234")
+                .transferToAccountId(2L)
+                .build();
+        assertThrows(InsufficientFundsException.class, () -> operationService.processOperation(operationRequestDTO));
+    }
+
+    @Test
+    void givenInvalidFundsConsumerId_thenNoAccountPresentException() {
+        OperationRequestDTO operationRequestDTO = OperationRequestDTO.builder()
+                .id(1L)
+                .operationType(OperationEnum.TRANSFER.getName())
+                .operationSum(BigDecimal.valueOf(10000))
+                .pin("1234")
+                .transferToAccountId(3L)
+                .build();
+        assertThrows(NoAccountPresentException.class, () -> operationService.processOperation(operationRequestDTO));
+    }
+
+    @Test
+    void givenInsufficientFunds_thenInsufficientFundsException() {
+        OperationRequestDTO operationRequestDTO = OperationRequestDTO.builder()
+                .id(1L)
+                .operationType(OperationEnum.WITHDRAW.getName())
+                .operationSum(BigDecimal.valueOf(30000))
+                .pin("1234")
+                .build();
+        assertThrows(InsufficientFundsException.class, () -> operationService.processOperation(operationRequestDTO));
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"withdraw", "Withrdaw"})
-    void processOperation_with_invalid_operations_name(String input){
-        OperationRequestDTO operationRequestDTO = OperationRequestDTO.builder().operationType(input).build();
+    @MethodSource("ru.aston.romanov.practical.services.OperationServiceTest#getValidOperationType")
+    void givenInvalidPinCode_thenThrowInvalidPinCodeException(String operationType) {
+        OperationRequestDTO operationRequestDTO = OperationRequestDTO.builder()
+                .id(1L)
+                .operationType(operationType)
+                .operationSum(BigDecimal.valueOf(10000))
+                .pin("1235")
+                .transferToAccountId(2L)
+                .build();
+        assertThrows(InvalidPinCodeException.class, () -> operationService.processOperation(operationRequestDTO));
+    }
+
+    @ParameterizedTest
+    @MethodSource("ru.aston.romanov.practical.services.OperationServiceTest#getValidOperationType")
+    void givenInvalidAccountId_thenThrowNoAccountPresentException(String operationType) {
+        OperationRequestDTO operationRequestDTO = OperationRequestDTO.builder()
+                .id(3L)
+                .operationType(operationType)
+                .operationSum(BigDecimal.valueOf(10000))
+                .pin("1234")
+                .transferToAccountId(2L)
+                .build();
+        assertThrows(NoAccountPresentException.class, () -> operationService.processOperation(operationRequestDTO));
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("ru.aston.romanov.practical.services.OperationServiceTest#getValidOperationType")
+    void givenValidOperationType(String operationType) {
+        OperationRequestDTO operationRequestDTO = OperationRequestDTO.builder()
+                .id(1L)
+                .operationType(operationType)
+                .operationSum(BigDecimal.valueOf(10000))
+                .pin("1234")
+                .transferToAccountId(2L)
+                .build();
+        TransactionDTO transactionDTO = operationService.processOperation(operationRequestDTO);
+        System.out.println(transactionDTO);
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("ru.aston.romanov.practical.services.OperationServiceTest#getInvalidOperationType")
+    @NullAndEmptySource
+    void giveInvalidOperationType_thenThrowsException(String operationType) {
+        OperationRequestDTO operationRequestDTO = OperationRequestDTO.builder()
+                .id(1L)
+                .operationType(operationType)
+                .operationSum(BigDecimal.valueOf(10000))
+                .pin("1234")
+                .build();
         assertThrows(UnsupportedAccountOperationException.class, () -> operationService.processOperation(operationRequestDTO));
     }
 
+    static Stream<Arguments> getInvalidOperationType() {
+        return Stream.of(
+                Arguments.of("MOVER"),
+                Arguments.of("Transfer"),
+                Arguments.of("transfer"),
+                Arguments.of("deposit"),
+                Arguments.of("DEPOSITE")
+        );
+    }
+
+    static Stream<Arguments> getValidOperationType() {
+        return Stream.of(
+                Arguments.of(OperationEnum.WITHDRAW.getName()),
+                Arguments.of(OperationEnum.DEPOSIT.getName()),
+                Arguments.of(OperationEnum.TRANSFER.getName())
+        );
+    }
 }
